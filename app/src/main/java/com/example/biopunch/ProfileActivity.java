c5e2da444f11b42847072ba342ef49a5fd2c98a2
janhavi544 <janhavisingh544@gmail.com>
2020-10-25 02:16:22 +0530

    profile

--- app/src/main/java/com/example/biopunch/ProfileActivity.java
+++ app/src/main/java/com/example/biopunch/ProfileActivity.java
@@ -3,12 +3,41 @@ package com.example.biopunch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        b1=findViewById(R.id.button1);
+        b2=findViewById(R.id.button2);
+        b3=findViewById(R.id.button3);
+        b4=findViewById(R.id.button4);
+    }
+    public void onClick(View view)
+    {
+        String a=view.getTag().toString();
+        if(a=="1")
+        {
+           b1.setBackgroundColor(272777);
+        }
+        if(a=="2")
+        {
+            b2.setBackgroundColor(272777);
+        }
+        if(a=="3")
+        {
+            b3.setBackgroundColor(272777);
+        }
+        if(a=="4")
+        {
+            b4.setBackgroundColor(272777);
+        }
     }
 }
