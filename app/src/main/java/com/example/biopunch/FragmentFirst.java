73cecfd3596477241ee554e0042b3b32d2e687cd
akki2000 <akshat.akki2000@gmail.com>
2020-10-28 02:00:14 +0530

    updated

--- app/src/main/java/com/example/biopunch/FragmentFirst.java
+++ app/src/main/java/com/example/biopunch/FragmentFirst.java
@@ -1,27 +1,47 @@
 package com.example.biopunch;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 
-public class FragmentFirst extends Fragment {
-
+public class FragmentFirst extends Fragment implements View.OnClickListener {
     public FragmentFirst() {}
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View v=inflater.inflate(R.layout.fragment_first, container, false);
+        Button b = (Button)v.findViewById(R.id.addEmployee);
+        b.setOnClickListener(this);
+        return v;
     }
-}
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addEmployee:
+              Intent intent=new Intent(getActivity(),AddEmployee.class);
+              getActivity().startActivity(intent);
+
+
+                break;
+        }
+    };
+
+    }
+
