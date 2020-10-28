0273134a7146a4dc2f6a2a5cb42bf401b26e61e9
janhavi544 <janhavisingh544@gmail.com>
2020-10-28 13:37:36 +0530

    add employee contact details

--- app/src/main/java/com/example/biopunch/AddEmployee.java
+++ app/src/main/java/com/example/biopunch/AddEmployee.java
@@ -1,17 +1,25 @@
 package com.example.biopunch;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.List;
 
-public class AddEmployee extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
+public class AddEmployee extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     String hr[];
     String min[];
     ArrayAdapter arrayAdapterInHr;
@@ -22,22 +30,32 @@ public class AddEmployee extends AppCompatActivity implements AdapterView.OnItem
     Spinner inMinSpinner;
     Spinner outHrSpinner;
     Spinner outMinSpinner;
+    EditText EmployeeNameEditText;
+    EditText EmployeePhoneEditText;
+
+    @Override
+    public void grantUriPermission(String toPackage, Uri uri, int modeFlags) {
+        super.grantUriPermission(toPackage, uri, modeFlags);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_employee);
         //initialising the arrays
-        hr=new String[24];
-        min=new String[60];
-        for(int i=0;i<60;i++)
-        {
-            String a=String.valueOf(i);
-           if(i<10)
-               a="0"+a;
-          if(i<24)
-               hr[i]=a;
-           min[i]=a;
+        hr = new String[24];
+        min = new String[60];
+        for (int i = 0; i < 60; i++) {
+            String a = String.valueOf(i);
+            if (i < 10)
+                a = "0" + a;
+            if (i < 24)
+                hr[i] = a;
+            min[i] = a;
         }
+        //editText
+        EmployeeNameEditText=findViewById(R.id.EmployeeNameEditText);
+        EmployeePhoneEditText=findViewById(R.id.EmployeePhoneEditText);
         //in: hour
         inHrSpinner = findViewById(R.id.inHrSpinner);
         inHrSpinner.setOnItemSelectedListener(this);
@@ -45,7 +63,7 @@ public class AddEmployee extends AppCompatActivity implements AdapterView.OnItem
         arrayAdapterInHr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         inHrSpinner.setAdapter(arrayAdapterInHr);
         //in: min
-        inMinSpinner=findViewById(R.id.inMinSpinner);
+        inMinSpinner = findViewById(R.id.inMinSpinner);
         inMinSpinner.setOnItemSelectedListener(this);
         arrayAdapterInMin = new ArrayAdapter(AddEmployee.this, android.R.layout.simple_spinner_item, min);
         arrayAdapterInMin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -57,7 +75,7 @@ public class AddEmployee extends AppCompatActivity implements AdapterView.OnItem
         arrayAdapterOutHr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         outHrSpinner.setAdapter(arrayAdapterOutHr);
         //out: min
-        outMinSpinner=findViewById(R.id.outMinSpinner);
+        outMinSpinner = findViewById(R.id.outMinSpinner);
         outMinSpinner.setOnItemSelectedListener(this);
         arrayAdapterOutMin = new ArrayAdapter(AddEmployee.this, android.R.layout.simple_spinner_item, min);
         arrayAdapterOutMin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -73,9 +91,40 @@ public class AddEmployee extends AppCompatActivity implements AdapterView.OnItem
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-    public void addEmployeeClicked(View view)
-    {
+
+    public void addEmployeeClicked(View view) {
         //save all this info and add this employee to the list view
 
+    }
+
+    public void chooseFromContacts(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case (1) :
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri contactData = data.getData();
+                    Cursor c =  getContentResolver().query(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+                        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        Toast.makeText(this, "Contact added:"+name, Toast.LENGTH_SHORT).show();
+                        EmployeeNameEditText.setText(name);
+                        if (Integer.parseInt(c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0)
+                        {
+                            // Query phone here. Covered next
+                            String ContctMobVar = c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            Log.i("Number", ContctMobVar);
+                            EmployeePhoneEditText.setText(ContctMobVar);
+                        }
+                    }
+                }
+                break;
+        }
     }
 }
